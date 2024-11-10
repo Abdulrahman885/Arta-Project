3511e3ce752d75b5b37cce2cec169d8785668dff
awad3317 <awadaaa3317@gmail.com>
2024-11-10 18:21:02 +0300

    Create UserRepository.php

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Repositories;
+use App\Interfaces\RepositoriesInterface;
+use App\Models\User;
+
+
+class UserRepository implements RepositoriesInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+    
+    public function index() : \Illuminate\Contracts\Pagination\LengthAwarePaginator
+    {
+        return User::paginate(10);
+    }
+    
+    public function getById($id) : User
+    {
+        return User::findOrFail($id);
+    }
+
+    public function store(array $data) : User
+    {
+        return User::create($data);
+    }
+
+    public function update(array $data,$id) : User
+    {
+        $User = $this->getById($id);
+        $User->update($data);
+        return $User;
+    }
+
+    public function delete($id) : bool
+    {
+        $User = $this->getById($id);
+        return $User->delete() > 0;
+    }
+}
