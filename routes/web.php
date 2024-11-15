a88a773fd774b833a5c9b289f1c3dc5245b62d00
awad3317 <awadaaa3317@gmail.com>
2024-11-15 13:45:02 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/awad', function () {
+Route::get('/auth', function () {
     return view('LOGIN');
 });
 
